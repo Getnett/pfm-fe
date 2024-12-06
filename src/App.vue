f350126787dd030f08a84ebea26d4d01982915f8
Getnett <getnetendaylalu2015@gmail.com>
2024-12-06 17:30:10 +0300

    init pfm-fe project

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Records</RouterLink>
+        <RouterLink to="/charts">Charts</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped></style>
---
+++ src/views/Charts.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1>This where we see different data charts for various transcation records</h1>
+</template>
