965c96ea2d62fce0d05ef01f3b762fa0c9a970dc
Getnett <getnetendaylalu2015@gmail.com>
2024-12-09 21:04:22 +0300

    connect with backend and css height adjustment

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,19 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Records</RouterLink>
-        <RouterLink to="/charts">Charts</RouterLink>
+  <div class="flex h-full">
+    <aside class="pt-8 px-8 bg-green-700 basis-60 [&_*]:text-white">
+      <nav class="flex flex-col">
+        <RouterLink class="hover:text-yellow-300" to="/">Records</RouterLink>
+        <RouterLink class="hover:text-yellow-300" to="/charts">Charts</RouterLink>
       </nav>
-    </div>
-  </header>
-  <RouterView />
-
-  <p>test if amazon amplify deploys Automatically after configuration - New build-Checkagain</p>
+    </aside>
+    <main class="flex-1 pt-8 px-8 bg-slate-100 [&_*]:text-black">
+      <div>
+        <RouterView />
+      </div>
+    </main>
+  </div>
 </template>
 
 <style scoped></style>
